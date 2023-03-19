439cc9377afb8747e4d5a5a5ad6b87c5b181df97
Tangang Mc Bride Tabufor <tangangbride@gmail.com>
2023-03-19 17:05:23 +0100

    Add HexColors and addColor functions

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-for(let i = 0; i < 10; i++) {
-   let box = document.createElement('div');
+window.onload = function() {
+   addColor();
+}
+
+for(let i = 0; i < 9; i++) {
+   const box = document.createElement('div');
    box.classList.add('box');
    document.querySelector('.container').appendChild(box);
+}
+ 
+const Btn = document.querySelector('.btn');
+const boxes = document.querySelectorAll('.box');
+
+function HexColors(){
+   let chars = '0123456789ABCDEF';
+   let colorLength = 6;
+   let color = '';
+   for(let i = 0; i < colorLength; i++){
+      let randomClor = (Math.floor(Math.random() * chars.length));
+      color += chars.substring(randomClor, randomClor + 1);
+   }
+return '#' + color;
+}
+
+function addColor(){
+   boxes.forEach(box => {
+      let newColor = HexColors();
+      box.style.backgroundColor = newColor;
+   });
 }
